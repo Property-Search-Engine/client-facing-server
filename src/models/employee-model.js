41da76740fa61eac62a8eb7a0ac024b085987bae
Víctor Martínez <victorcoder2@gmail.com>
2021-02-08 12:38:24 +0100

    preparing environment

--- src/models/employee-model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const EmployeeSchema = require("./schemas/employee")
-
-const Employee = mongoose.model("Employee", EmployeeSchema);
-
-module.exports = Employee;
--- src/server.js
+++ src/server.js
@@ -8,8 +8,6 @@ const auth = require("./middleware/auth-middleware")
 const app = express();
 
 const errorMiddleware = require("./middleware/error-middleware");
-const userRouter = require("./routes/user-routes");
-const propertyRouter = require("./routes/property-routes");
 
 app.use(morgan("dev"));
 app.use(helmet());
@@ -17,8 +15,7 @@ app.use(json());
 app.use(cors());
 
 app.use(auth());
-app.use("/user", userRouter);
-app.use("/properties", propertyRouter);
+//app.use("/user", userRouter);
 app.use(errorMiddleware);
 
 module.exports = app;
