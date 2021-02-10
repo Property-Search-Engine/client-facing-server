1dc8b4a0a1364df86e0b4bc757807fa193b3b840
Víctor Martínez <victorcoder2@gmail.com>
2021-02-10 10:54:14 +0100

    auth middleware removed from serverjs

--- src/server.js
+++ src/server.js
@@ -3,9 +3,7 @@ const morgan = require("morgan");
 const { json } = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
-const auth = require("./middleware/auth-middleware")
 const bookingsRouter = require("./routes/bookings-routes");
-const adminsRouter = require("./routes/admin-serv-routes");
 const errorMiddleware = require("./middleware/error-middleware");
 
 const app = express();
@@ -16,7 +14,6 @@ app.use(helmet());
 app.use(json());
 app.use(cors());
 
-app.use(auth());
 app.use("/bookings", bookingsRouter);
 //app.use("/user", userRouter);
 app.use(errorMiddleware);
