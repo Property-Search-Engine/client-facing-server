7f18af5a18591113584363c5c771dd55e91b4c45
devmxhv <mahmoud.mostafa3107@gmail.com>
2021-02-11 13:51:04 +0100

    Fixed bugs

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,5 @@
 const logger = require("loglevel");
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 
 logger.enableAll();
 
@@ -9,7 +8,7 @@ const {
   MONGO_DB_URL_PRODUCTION,
   MONGO_DB_URL_DEVELOPMENT,
   MONGO_DB_URL_TEST,
-  PORT = 5001,
+  PORT = 5000,
   JWT_SECRET_PAYLOAD,
   JWT_SECRET_SIGN,
   ADMIN_SERVER_URL,
